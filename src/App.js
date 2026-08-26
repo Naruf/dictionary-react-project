e147cb556bc9d79f985c46a10443a07a33120ab0
Naruf <102542599+Naruf@users.noreply.github.com>
2026-08-26 17:21:11 +0200

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,43 +1,14 @@
 import logo from "./naruf-dev_logo_horizontal_trimmed.svg";
 import "./App.css";
+import Footer from "./Footer";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header ">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="btn-primary">
-          This app was coded by{" "}
-          <a
-            className="App-link f-1"
-            href="https://github.com/Naruf"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nadia Ruiz
-          </a>
-          , is open source in{" "}
-          <a
-            className="App-link"
-            href="https://github.com/Naruf/dictionary-react-project"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Github
-          </a>{" "}
-          and hosted in{" "}
-          <a
-            className="App-link"
-            href="https://vercel.com/narufs-projects/dictionary-react-project"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vercel
-          </a>
-        </p>
       </header>
+      <Footer />
     </div>
   );
 }
-
-export default App;
---
+++ src/Footer.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      This app was coded by{" "}
+      <a
+        className="Footer-link"
+        href="https://github.com/Naruf"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Nadia Ruiz
+      </a>
+      , is open-sourced in{" "}
+      <a
+        className="Footer-link"
+        href="https://github.com/Naruf/dictionary-react-project"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Github
+      </a>{" "}
+      and hosted in{" "}
+      <a
+        className="Footer-link"
+        href="https://vercel.com/narufs-projects/dictionary-react-project"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Vercel
+      </a>
+    </div>
+  );
+}
