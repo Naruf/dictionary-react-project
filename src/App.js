d9127ee75dc74bbd54c0f7b024fc881aad61f635
Naruf <102542599+Naruf@users.noreply.github.com>
2026-08-26 14:57:27 +0200

    Projest setup
    Project setup:
    - React app creation
    - Removing unnecessary code
    - Logo added and some text
    - Bootstrap installation
    - Axios installation
    - Responsive behaviour

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import logo from "./naruf-dev_logo_horizontal_trimmed.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header ">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="btn-primary">
+          This app was coded by{" "}
+          <a
+            className="App-link f-1"
+            href="https://github.com/Naruf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Nadia Ruiz
+          </a>
+          , is open source in{" "}
+          <a
+            className="App-link"
+            href="https://github.com/Naruf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>{" "}
+          and hosted in{" "}
+          <a
+            className="App-link"
+            href="https://github.com/Naruf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vercel
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
